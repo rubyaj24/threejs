4cd92a71c600b7cd5594029737ed6815a15fe9cc
rubyaj24 <amaljithmvinod@gmail.com>
2025-06-18 14:39:56 +0530

    Added title and textures

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 import getStarfield from './getStarField.js';
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
@@ -12,30 +12,51 @@ camera.position.z = 5;
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const earthGroup = new THREE.Group();
-earthGroup.rotation.z = 23.4 * Math.PI / 180;
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
 scene.add(earthGroup);
 const geo = new THREE.IcosahedronGeometry(1.0, 8);
-const mat = new THREE.MeshStandardMaterial({
-    map: new THREE.TextureLoader().load('/textures/590923.jpg'),
-    // color: 0xccff,
-    // flatShading: true,
+const mat = new THREE.MeshPhongMaterial({
+    map: new THREE.TextureLoader().load("/textures/earthmap1k.jpg"),
+    specularMap: new THREE.TextureLoader().load("/textures/02_earthspec1k.jpg"),
+    bumpMap: new THREE.TextureLoader().load("/textures/earthbump1k.jpg"),
+    bumpScale: 0.04,
 });
 
 const mesh = new THREE.Mesh(geo, mat);
 earthGroup.add(mesh);
 
+const lightMat = new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load('/textures/earthlights1k.jpg'),
+    blending: THREE.AdditiveBlending,
+});
+
+const lightMesh = new THREE.Mesh(geo, lightMat);
+earthGroup.add(lightMesh);
+
 const stars = getStarfield({numStars: 20000});
 scene.add(stars);
 
-scene.add(mesh);
+// scene.add(mesh);
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
-scene.add(hemiLight);
+const sunlight = new THREE.DirectionalLight(0xffffff);
+sunlight.position.set(-2,0.5,1.5);
+scene.add(sunlight);
+
+// const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
+// scene.add(hemiLight);
 
 function animate() {
     requestAnimationFrame(animate);
     mesh.rotation.y += 0.001;
+    lightMesh.rotation.y += 0.001;
     renderer.render(scene, camera);
 }
 
 animate();
+
+function handleWindowResize () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+  window.addEventListener('resize', handleWindowResize, false);
